0364ce5db015d48dff8e5f2b0c17f51e8da8210a
PawnISEP <mesoponions@gmail.com>
2025-01-13 09:08:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 #Importation de Flask et de ses modules
 from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
 from flask import render_template
+from werkzeug.utils import safe_join
 from flask import json
 from urllib.request import urlopen
 from werkzeug.utils import secure_filename
