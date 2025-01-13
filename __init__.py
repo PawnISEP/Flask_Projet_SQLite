6df475e5422948fd7d37dc3f7eb3709fa250cbfe
PawnISEP <mesoponions@gmail.com>
2025-01-13 09:11:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,10 @@
 #Importation de Flask et de ses modules
-from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
-from flask import render_template
-from werkzeug.utils import safe_join
-from flask import json
+from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session, json
 from urllib.request import urlopen
 from werkzeug.utils import secure_filename
+from werkzeug.utils import safe_join
 from datetime import datetime
 import sqlite3
-
 
 app = Flask(__name__)
 
